45a04dae2157a6bfb825dcfba174ed9a9f2794e7
MyronTheDeveloper66 <chilldudelegend@gmail.com>
2018-03-04 00:53:56 -0600

    modified App and Header components for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 class App extends Component {
   render() {
     return (
-      <h1>My App</h1>
+      <div className = "myApp">
+        <h1>My App</h1>
+        <Header />
+      </div>
     );
   }
 }
